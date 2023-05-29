0ec02ef07487a9628aa2aec2751f24e2b973050f
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-05-28 21:30:41 -0600

    navbar component complete

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 
 const Navbar = () => {
@@ -23,7 +24,8 @@ const Navbar = () => {
                         "
                     >
                         <Logo />
-                        
+                        <Search />
+
                     </div>
                 </Container>
             </div>

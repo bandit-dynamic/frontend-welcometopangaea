2d98393e16ed13bc09d75625047474c9755c9e38
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-05-29 21:12:08 -0600

    started making a welcome button

---
+++ app/components/Button.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { IconType } from 'react-icons';
+
+interface ButtonProps {
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    disabled?: boolean;
+    outline?: boolean;
+    small?: boolean;
+    icon?: IconType;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    disabled,
+    outline,
+    small,
+    icon
+}) => {
+    return (
+        <button
+            className={`
+            relative
+            disabled:opacity-70
+            disabled:cursor-not-allowed
+            hover:opacity-80
+            transition
+            w-full
+            ${outline ? 'bg-white' : 'bg-yellow-600'}
+            ${outline ? 'bg-white' : 'bg-yellow-600'}
+            ${outline ? 'bg-white' : 'bg-yellow-600'}
+            `}
+        >
+           {label} 
+        </button>
+    );
+}
+
+export default Button

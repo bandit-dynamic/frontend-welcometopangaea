59f7a70a8e54826fa951422a26ffe7f7366e3b71
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-06-03 19:12:48 -0600

    successfully uploaded images with Cloudinary API

--- app/components/modals/SellModal.tsx
+++ app/components/modals/SellModal.tsx
@@ -9,8 +9,12 @@ import useSellModal from "@/app/hooks/useSellModal";
 import Modal from "./Modal";
 import Heading from '../Heading';
 import { categories } from "../navbar/Categories";
+
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
+import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload"
+
 import dynamic from "next/dynamic";
 
 
@@ -41,6 +45,7 @@ const SellModal = () => {
         defaultValues: {
             category: '',
             location: null,
+            acreageCount: 1,
             imageSrc: '',
             price: 1,
             titel: '',
@@ -48,95 +53,131 @@ const SellModal = () => {
         }
     });
 
-    const category = watch('category');    
-    const location = watch('location');
+const category = watch('category');    
+const location = watch('location');
+const acreageCount = watch('acreageCount');
+const imageSrc = watch('imageSrc');
 
-    const Map = useMemo(() => dynamic(() => import('../Map'), {
-        ssr: false
-    }), [location]);
+const Map = useMemo(() => dynamic(() => import('../Map'), {
+    ssr: false
+}), [location]);
 
 
-    const setCustomValue = (id: string, value: any) => {
-        setValue(id, value, {
-            shouldValidate: true,
-            shouldDirty: true,
-            shouldTouch: true,
-        })
+const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+        shouldValidate: true,
+        shouldDirty: true,
+        shouldTouch: true,
+    })
+}
+
+const onBack = () => {
+    setStep((value) => value -1);
+};
+
+const onNext = () => {
+    setStep((value) => value +1);
+};
+
+const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE){
+        return 'Create';
     }
 
-    const onBack = () => {
-        setStep((value) => value -1);
-    };
+    return 'Next';
+}, [step]);
 
-    const onNext = () => {
-        setStep((value) => value +1);
-    };
+const secondaryActionLabel = useMemo (() => {
+    if (step === STEPS.CATEGORY) {
+        return undefined
+    }
 
-    const actionLabel = useMemo(() => {
-        if (step === STEPS.PRICE){
-            return 'Create';
-        }
+    return 'Back'
+}, [step]);
 
-        return 'Next';
-    }, [step]);
+let bodyContent = (
+    <div className="flex flex-col gap-8">
+        <Heading
+            title="Which location best describes your property?"
+            subtitle="Pick a category"
+    />
+    <div
+        className="
+            grid
+            grid-cols-1
+            md:grid-cols-2
+            gap-3
+            max-h-[50vh]
+            overflow-y-auto
+        "
+    >
+        {categories.map ((item) => (
+            <div key={item.label} className="col-span-1">
+                <CategoryInput
+                    onClick={(category) => 
+                        setCustomValue('category', category)}
+                    selected={category === item.label}
+                    label={item.label}
+                    icon={item.icon}
+                />
 
-    const secondaryActionLabel = useMemo (() => {
-        if (step === STEPS.CATEGORY) {
-            return undefined
-        }
+            </div>
+                ))}
+            </div>
+        </div>
+    )
 
-        return 'Back'
-    }, [step]);
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your property located?"
+                    subtitle="Help buyers find you!"
+                />
+                <CountrySelect 
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}                      
+                />
+                <Map 
+                    center={location?.latlng}
+                />
+            </div>
+        )
+    }
 
-    let bodyContent = (
-        <div className="flex flex-col gap-8">
-            <Heading
-                title="Which location best describes your property?"
-                subtitle="Pick a category"
+    if (step === STEPS.INFO) {
+        bodyContent= (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Basics about your property"
+                    subtitle="What are some nearby activities and features?"
             />
-            <div
-                className="
-                    grid
-                    grid-cols-1
-                    md:grid-cols-2
-                    gap-3
-                    max-h-[50vh]
-                    overflow-y-auto
-                "
-            >
-                {categories.map ((item) => (
-                    <div key={item.label} className="col-span-1">
-                        <CategoryInput
-                            onClick={(category) => 
-                                setCustomValue('category', category)}
-                            selected={category === item.label}
-                            label={item.label}
-                            icon={item.icon}
-                        />
+            <Counter 
+                title="Acreage:"
+                subtitle="How many acres is your parcel?"
+                value={acreageCount}
+                onChange={(value) => setCustomValue('acreageCount', value)}
+            />
+            </div>
+        )
+    }
 
-                    </div>
-                        ))}
-                    </div>
-                </div>
-            )
+    if (step === STEPS.IMAGES) {
+        bodyContent= (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Add photos"
+                    subtitle="Show buyers your land"
+                />
+                <ImageUpload 
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                />
+            </div>
+        )
+    }
 
-            if (step === STEPS.LOCATION) {
-                bodyContent = (
-                    <div className="flex flex-col gap-8">
-                        <Heading
-                            title="Where is your property located?"
-                            subtitle="Help buyers find you!"
-                        />
-                        <CountrySelect 
-                            value={location}
-                          onChange={(value) => setCustomValue('location', value)}                      
-                        />
-                        <Map 
-                            center={location?.latlng}
-                        />
-                    </div>
-                )
-            }
+
 
     return ( 
         <Modal 

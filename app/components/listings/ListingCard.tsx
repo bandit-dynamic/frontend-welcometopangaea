28aca7bab169627319ee7bd9cf4ee01d0bcbe087
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-06-05 19:17:36 -0600

    successfully rendered/styled listing card onto index from DB listing model object

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,105 +1,138 @@
 'use client';
 
-import { Listing, Inquiry } from "@prisma/client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
-import Image from "nexxt/image";
-
-import { SafeUser } from "@/app/types";
 
 import useCountries from "@/app/hooks/useCountries";
+import { 
+  SafeListing, 
+  SafeInquiry, 
+  SafeUser 
+} from "@/app/types";
 
+// import HeartButton from "../HeartButton";
+import Button from "../Button";
+import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
-    data: Listing;
-    inquiry?: Inquiry;
-    onAction?: (id: string) => void;
-    disabled?: boolean;
-    actionLabel?: string;
-    actionId?: string;
-    currentUser?: SafeUser | null;
-}
+  data: SafeListing;
+  inquiry?: SafeInquiry;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null
+};
 
-
-const ListingCard: React.FC<ListingCardProps>  = ({
-    data,
-    inquiry,
-    onAction,
-    disabled,
-    actionLabel,
-    actionId = "",
-    currentUser
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  inquiry,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = '',
+  currentUser,
 }) => {
-    const router = useRouter ();
-    const { getByValue } = useCountries();
+  const router = useRouter();
+  const { getByValue } = useCountries();
 
-    // const location = getByValue(data.locationValue);
+  const location = getByValue(data.locationValue);
 
-    const handleCancel = useCallback(
-        (e: React.MouseEvent<HTMLButtonElement>) => {
-            e.stopPropagation();
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
 
-            if (disabled) {
-                return;
-            }
+    if (disabled) {
+      return;
+    }
 
-            onAction?.(actionId);
-        }, [onAction, actionId, disabled])
+    onAction?.(actionId)
+  }, [disabled, onAction, actionId]);
 
-        const price = useMemo(() => {
-            if (inquiry) {
-                return inquiry.proposedPrice;
-            }
+  const price = useMemo(() => {
+    if (inquiry) {
+      return inquiry.proposedPrice;
+    }
 
-            return data.price;
-        }, [inquiry, data.price]);
+    return data.price;
+  }, [inquiry, data.price]);
 
-        const inquiryDate = useMemo(() => {
-            if (!inquiry) {
-                return null;
-            }
+  const inquiryDate = useMemo(() => {
+    if (!inquiry) {
+      return null;
+    }
+  
+    const start = new Date(inquiry.startOfferDate);
+    const end = new Date(inquiry.endOfferDate);
 
-            // const start = new Date(inquiry.offerStartDate)
-            // const end = new Date(inquiry.offerThruDate)
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+  }, [inquiry]);
 
-            return `${format(start, 'PP')} - ${format(end, 'PP')}`
-        }, [inquiry])
-
-    return (
-        <div
-            onClick={() => router.push(`/listings/${data.id}`)}
-            className="
-                col-span-1 cursor-pointer group
-            
-            "
+  return (
+    <div 
+      onClick={() => router.push(`/listings/${data.id}`)} 
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div 
+          className="
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          "
         >
-            <div className="flex flex-col gap-2 w-full">
-                <div
-                    className="
-                        aspect-square
-                        w-full
-                        relative
-                        overflow-hidden
-                        rounded-xl
-                    "
-                >
-                    <image 
-                        fill
-                        alt="Listing"
-                        src={data.imageSrc}
-                        className="
-                            object-cover
-                            h-full
-                            w-full
-                            group-hover:scale-110
-                            transition
-                        "
-                    />
-                </div>
-            </div>
+          <Image
+            fill
+            className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+            src={data.imageSrc}
+            alt="Listing"
+          />
+          <div className="
+            absolute
+            top-3
+            right-3
+          ">
+            {/* <HeartButton 
+              listingId={data.id} 
+              currentUser={currentUser}
+            /> */}
+          </div>
         </div>
-    );
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {inquiryDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">
+            $ {price}
+          </div>
+          {!inquiry && (
+            <div className="font-light"></div>
+          )}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel} 
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+   );
 }
-
+ 
 export default ListingCard;

fe279c7b74fc21a3694454589add92c5ee8e267f
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-06-07 17:57:05 -0600

    data.imageSrc debug v1

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -94,7 +94,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
               group-hover:scale-110 
               transition
             "
-            src={data.imageSrc}
+            src={data.imageSrc || '/default-image.jpg'}
             alt="Listing"
           />
           <div className="
